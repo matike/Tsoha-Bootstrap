9af6dfdac0d7b50fdf5cc06a9976a46d2b6a1bbd
matike <a@a.com>
2015-03-22 18:24:41 +0200

    paivitys

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,30 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/tilaus', function() {
+    HelloWorldController::tilaus();
+});
+
+$routes->get('/laakarit', function() {
+    HelloWorldController::laakarit();
+});
+
+$routes->get('/laakari', function() {
+    HelloWorldController::laakari();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/register', function() {
+    HelloWorldController::register();
+});
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,43 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Doctor(
+id SERIAL PRIMARY KEY,
+doctor_name varchar(100) NOT NULL,
+phone varchar(15) NOT NULL,
+email varchar(100) NOT NULL,
+password varchar(50) NOT NULL
+);
+
+CREATE TABLE Patient(
+id SERIAL PRIMARY KEY,
+patient_name varchar(100) NOT NULL,
+address varchar(100) NOT NULL,
+email varchar(100) NOT NULL,
+password varchar(50) NOT NULL
+);
+
+CREATE TABLE History(
+id SERIAL PRIMARY KEY,
+patient_id INTEGER REFERENCES Patient(id),
+history text
+);
+
+CREATE TABLE Visit(
+id SERIAL PRIMARY KEY,
+patient_id INTEGER REFERENCES Patient(id),
+doctor_id INTEGER REFERENCES Doctor(id),
+visit_date date
+);
+
+CREATE TABLE Report(
+id SERIAL PRIMARY KEY,
+visit_id INTEGER REFERENCES Visit(id),
+doctor_id INTEGER REFERENCES Doctor(id),
+patient_id INTEGER REFERENCES Patient(id),
+description text NOT NULL
+);
+
+CREATE TABLE Regimen(
+id SERIAL PRIMARY KEY,
+patient_id INTEGER REFERENCES Patient(id),
+regimen text NOT NULL,
+regimen_date date
+);
